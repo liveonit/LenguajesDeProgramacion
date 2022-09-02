47fa10492e32e1d6772137d17367d619e2532d52
joaquinpereyra <joaquin.pereyrar@correo.ucu.edu.uy>
2022-09-02 20:41:51 -0300

    operaciones

--- stackalc/src/main.rs
+++ stackalc/src/main.rs
@@ -4,11 +4,6 @@ let mut operations: [&str; 5] = ["SUB", "ADD", "MULT", "RET", "DIV"];
 fn main() {
 
 }
-
-type op
-
-
-
 fn stackalc() {
 
     let mut lines = io::stdin().lock().lines();
@@ -28,4 +23,76 @@ fn stackalc() {
             }
         }
     }
+}
+
+fn add(mut stack : Vec<f64>) -> Vec<f64> {
+    if (stack.len() > 1) {
+        let item1 : f64 = match stack.pop() {
+            Some(f64) => f64,
+            None => 0.0
+        };
+        let item2 : f64 = match stack.pop() {
+            Some(f64) => f64,
+            None => 0.0
+        };
+        let result = item1 + item2;
+        stack.push(result)
+    }
+    return stack;
+
+}
+
+fn mult(mut stack : Vec<f64>) -> Vec<f64> {
+    if (stack.len() > 1) {
+        let item1 : f64 = match stack.pop() {
+            Some(f64) => f64,
+            None => 1.0
+        };
+        let item2 : f64 = match stack.pop() {
+            Some(f64) => f64,
+            None => 1.0
+        };
+        let result = item1 * item2;
+        stack.push(result)
+    }
+    return stack;
+
+}
+
+fn div(mut stack : Vec<f64>) -> Vec<f64> {
+    if (stack.len() > 1) {
+        let item1 : f64 = match stack.pop() {
+            Some(f64) => f64,
+            None => 1.0
+        };
+        let item2 : f64 = match stack.pop() {
+            Some(f64) => f64,
+            None => 1.0
+        };
+        let result =  item2 / item1;
+        stack.push(result)
+    }
+    return stack;
+
+}
+
+fn sub(mut stack : Vec<f64>) -> Vec<f64> {
+    if (stack.len() > 1) {
+        let item1 : f64 = match stack.pop() {
+            Some(f64) => f64,
+            None => 0.0
+        };
+        let item2 : f64 = match stack.pop() {
+            Some(f64) => f64,
+            None => 0.0
+        };
+        let result = item2 - item1;
+        stack.push(result)
+    }
+    return stack;
+
+}
+
+fn ret(mut stack : Vec<f64>) -> Vec<f64> {
+    return stack;
 }
