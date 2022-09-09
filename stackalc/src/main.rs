066e84b52fa31ce4553da01dfe19cf46c1907442
liveofit <ibarretorey@gmail.com>
2022-09-09 20:54:36 -0300

    add new features to jump in instructions

--- stackalc/src/main.rs
+++ stackalc/src/main.rs
@@ -3,9 +3,8 @@ use std::io::{self, BufRead};
 use std::*;
 use std::process::exit;
 
-
 fn main() {
-    let mut operations: HashMap<&str, fn(Vec<f64>, [f64; 10], usize) -> (Vec<f64>, [f64; 10])> = HashMap::new();
+    let mut operations: HashMap<&str, fn(Vec<f64>, [f64; 10], i32, usize) -> (Vec<f64>, [f64; 10], usize)> = HashMap::new();
     operations.insert("ADD", add);
     operations.insert("SUB", sub);
     operations.insert("MULT", mult);
@@ -23,12 +22,14 @@ fn main() {
     operations.insert("POP", pop);
     operations.insert("GET", get);
     operations.insert("SET", set);
+    operations.insert("UJP", ujp);
+    operations.insert("CJP", cjp);
 
     stackalc(operations);
 }
 
 
-fn stackalc(operations: HashMap<&str, fn(Vec<f64>, [f64; 10], usize) -> (Vec<f64>, [f64; 10])>) {
+fn stackalc(operations: HashMap<&str, fn(Vec<f64>, [f64; 10], i32, usize) -> (Vec<f64>, [f64; 10], usize)>) {
     let mut stack : Vec<f64> = Vec::new();
     let mut variables: [f64; 10] = [f64::NAN; 10];
     let mut lines = io::stdin().lock().lines();
@@ -40,25 +41,29 @@ fn stackalc(operations: HashMap<&str, fn(Vec<f64>, [f64; 10], usize) -> (Vec<f64
           println!("Variables {:?}", variables);
           exit(0);
         } else {
-            for val in last_input.split_whitespace() {
-               let inputs = val.split(':').collect::<Vec<&str>>();
+            let input = last_input.split_whitespace().collect::<Vec<&str>>();
+            let mut counter: usize = 0;
+            while counter < input.len()  {
+                let inputs = input[counter].split(':').collect::<Vec<&str>>();
                 if operations.contains_key(inputs[0]) {
                     let op = operations[inputs[0]];
-                    let n = if inputs.len() > 1 {  inputs[1].parse().unwrap()} else { usize::MIN };
-                    (stack, variables) = op(stack, variables, n);
+                    let n = if inputs.len() > 1 {  inputs[1].parse().unwrap()} else { i32::MIN };
+                    (stack, variables, counter) = op(stack, variables, n, counter);
                 } else {
-                  let n:f64 = val.parse().unwrap();
+                  let n:f64 = input[counter].parse().unwrap();
                   stack.push(n);
+                  counter = counter + 1;
                 }
-              }
-            println!("Variables {:?}", variables);
-            println!("Stack {:?}", stack);
+            }
         }
     }
+    println!("Stack: {:?}", stack);
+    println!("Variables {:?}", variables);
+
 }
 
 
-fn add(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64; 10]) {
+fn add(mut stack : Vec<f64>, variables: [f64; 10], _n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
     if stack.len() > 1 {
         let item1 : f64 = match stack.pop() {
             Some(f64) => f64,
@@ -71,11 +76,10 @@ fn add(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64
         let result = item1 + item2;
         stack.push(result)
     }
-    return (stack, variables);
-
+    return (stack, variables, counter + 1);
 }
 
-fn mult(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64; 10]) {
+fn mult(mut stack : Vec<f64>, variables: [f64; 10], _n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
     if stack.len() > 1 {
         let item1 : f64 = match stack.pop() {
             Some(f64) => f64,
@@ -88,11 +92,11 @@ fn mult(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f6
         let result = item1 * item2;
         stack.push(result)
     }
-    return (stack, variables);
+    return (stack, variables, counter + 1);
 
 }
 
-fn div(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64; 10]) {
+fn div(mut stack : Vec<f64>, variables: [f64; 10], _n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
     if stack.len() > 1 {
         let item1 : f64 = match stack.pop() {
             Some(f64) => f64,
@@ -105,10 +109,10 @@ fn div(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64
         let result =  item2 / item1;
         stack.push(result)
     }
-    return (stack, variables);
+    return (stack, variables, counter + 1);
 }
 
-fn sub(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64; 10]) {
+fn sub(mut stack : Vec<f64>, variables: [f64; 10], _n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
     if stack.len() > 1 {
         let item1 : f64 = match stack.pop() {
             Some(f64) => f64,
@@ -121,10 +125,10 @@ fn sub(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64
         let result = item2 - item1;
         stack.push(result)
     }
-    return (stack, variables);
+    return (stack, variables, counter + 1);
 }
 
-fn eq(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64; 10]) {
+fn eq(mut stack : Vec<f64>, variables: [f64; 10], _n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
     if stack.len() > 1 {
         let item1 : f64 = match stack.pop() {
             Some(f64) => f64,
@@ -141,10 +145,10 @@ fn eq(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64;
             stack.push(0.0);
         }
     }
-    return (stack, variables);
+    return (stack, variables, counter + 1);
 }
 
-fn diff(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64; 10]) {
+fn diff(mut stack : Vec<f64>, variables: [f64; 10], _n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
     if stack.len() > 1 {
         let item1 : f64 = match stack.pop() {
             Some(f64) => f64,
@@ -161,10 +165,10 @@ fn diff(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f6
             stack.push(0.0);
         }
     }
-    return (stack, variables);
+    return (stack, variables, counter + 1);
 }
 
-fn lt(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64; 10]) {
+fn lt(mut stack : Vec<f64>, variables: [f64; 10], _n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
     if stack.len() > 1 {
         let item1 : f64 = match stack.pop() {
             Some(f64) => f64,
@@ -181,10 +185,10 @@ fn lt(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64;
             stack.push(0.0);
         }
     }
-    return (stack, variables);
+    return (stack, variables, counter + 1);
 }
 
-fn lte(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64; 10]) {
+fn lte(mut stack : Vec<f64>, variables: [f64; 10], _n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
     if stack.len() > 1 {
         let item1 : f64 = match stack.pop() {
             Some(f64) => f64,
@@ -201,10 +205,10 @@ fn lte(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64
             stack.push(0.0);
         }
     }
-    return (stack, variables);
+    return (stack, variables, counter + 1);
 }
 
-fn gt(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64; 10]) {
+fn gt(mut stack : Vec<f64>, variables: [f64; 10], _n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
     if stack.len() > 1 {
         let item1 : f64 = match stack.pop() {
             Some(f64) => f64,
@@ -221,10 +225,10 @@ fn gt(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64;
             stack.push(0.0);
         }
     }
-    return (stack, variables);
+    return (stack, variables, counter + 1);
 }
 
-fn gte(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64; 10]) {
+fn gte(mut stack : Vec<f64>, variables: [f64; 10], _n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
     if stack.len() > 1 {
         let item1 : f64 = match stack.pop() {
             Some(f64) => f64,
@@ -241,10 +245,10 @@ fn gte(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64
             stack.push(0.0);
         }
     }
-    return (stack, variables);
+    return (stack, variables, counter + 1);
 }
 
-fn and(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64; 10]) {
+fn and(mut stack : Vec<f64>, variables: [f64; 10], _n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
     if stack.len() > 1 {
         let item1 : f64 = match stack.pop() {
             Some(f64) => f64,
@@ -257,10 +261,10 @@ fn and(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64
         let result = if item1!=0.0 && !item1.is_nan()  {item1} else {item2};
         stack.push(result)
     }
-    return (stack, variables);
+    return (stack, variables, counter + 1);
 }
 
-fn or(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64; 10]) {
+fn or(mut stack : Vec<f64>, variables: [f64; 10], _n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
     if stack.len() > 1 {
         let item1 : f64 = match stack.pop() {
             Some(f64) => f64,
@@ -273,10 +277,10 @@ fn or(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64;
         let result = if item1!=0.0 && !item1.is_nan()  {item2} else {item1};
         stack.push(result)
     }
-    return (stack, variables);
+    return (stack, variables, counter + 1);
 }
 
-fn not(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64; 10]) {
+fn not(mut stack : Vec<f64>, variables: [f64; 10], _n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
     if stack.len() > 0 {
         let item1 : f64 = match stack.pop() {
             Some(f64) => f64,
@@ -285,27 +289,45 @@ fn not(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64
         let result: f64 = if item1==0.0 {1.0} else {0.0};
         stack.push(result)
     }
-    return (stack, variables);
+    return (stack, variables, counter + 1);
 }
 
-fn dup(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64; 10]) {
+fn dup(mut stack : Vec<f64>, variables: [f64; 10], _n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
     let item : f64 = stack.pop().unwrap();
     stack.push(item);
     stack.push(item);
-    return (stack, variables);
+    return (stack, variables, counter + 1);
 }
 
-fn pop(mut stack : Vec<f64>, variables: [f64; 10], _n: usize) -> (Vec<f64>, [f64; 10]) {
+fn pop(mut stack : Vec<f64>, variables: [f64; 10], _n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
     stack.pop();
-    return (stack, variables);
+    return (stack, variables, counter + 1);
 }
 
-fn get(mut stack : Vec<f64>, variables: [f64; 10], n: usize) -> (Vec<f64>, [f64; 10]) {
-    stack.push(variables[n]);
-    return (stack, variables);
+fn get(mut stack : Vec<f64>, variables: [f64; 10], n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
+    stack.push(variables[n as usize]);
+    return (stack, variables, counter + 1);
 }
-fn set(mut stack : Vec<f64>, mut variables: [f64; 10], n: usize) -> (Vec<f64>, [f64; 10]) {
+fn set(mut stack : Vec<f64>, mut variables: [f64; 10], n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
     let item : f64 = stack.pop().unwrap();
-    variables[n] = item;
-    return (stack, variables);
+    variables[n as usize] = item;
+    return (stack, variables, counter + 1);
+}
+
+fn ujp(stack : Vec<f64>, variables: [f64; 10], n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
+  if n < 0 {
+    return (stack, variables, counter - ((n * -1) as usize));
+  }
+  return (stack, variables, counter + n as usize);
+}
+
+fn cjp(mut stack : Vec<f64>, variables: [f64; 10], n: i32, counter: usize) -> (Vec<f64>, [f64; 10], usize) {
+    let item : f64 = stack.pop().unwrap();
+    if item > 0.0 {
+        if n < 0 {
+          return (stack, variables, counter - ((n * -1) as usize));
+        }
+        return (stack, variables, counter + n as usize);
+    }
+    return (stack, variables, counter + 1);
 }
