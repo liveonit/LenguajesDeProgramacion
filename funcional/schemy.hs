4280aa6db189453a2b276998e453dadae3e44342
Antonella Gutierrez <antonellagutierrez@adinet.com.uy>
2022-09-23 20:27:11 -0300

    ejercicio 2.1 y 2.2

--- funcional/schemy.hs
+++ funcional/schemy.hs
@@ -1,4 +1,14 @@
+import Data.Map.Strict (Map, fromList)
+import Data.Map ((!))
+
 data SchemyExp = SchemyNumber Double
   | SchemyAdd SchemyExp SchemyExp
   | SchemyMult SchemyExp SchemyExp
+  | SchemySymbol String
   deriving (Eq, Show)
+
+eval :: (Map String Double) -> SchemyExp -> Double
+eval map (SchemyNumber n) = n
+eval map (SchemyAdd a b) = eval map a + eval map b
+eval map (SchemyMult a b) = eval map a * eval map b
+eval map (SchemySymbol s) = map ! s
