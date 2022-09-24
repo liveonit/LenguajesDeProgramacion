83785b58a4a1a4a2763886a975a892403f9765ca
Germ90 <germanrios90@gmail.com>
2022-09-23 21:05:36 -0300

    Update schemy.hs

--- funcional/schemy.hs
+++ funcional/schemy.hs
@@ -5,10 +5,16 @@ data SchemyExp = SchemyNumber Double
   | SchemyAdd SchemyExp SchemyExp
   | SchemyMult SchemyExp SchemyExp
   | SchemySymbol String
+  | SchemyBool Bool
   deriving (Eq, Show)
 
-eval :: (Map String Double) -> SchemyExp -> Double
-eval map (SchemyNumber n) = n
-eval map (SchemyAdd a b) = eval map a + eval map b
-eval map (SchemyMult a b) = eval map a * eval map b
+eval :: (Map String SchemyExp) -> SchemyExp -> SchemyExp
+eval map (SchemyNumber n) = SchemyNumber n
+eval map (SchemyAdd a b) = SchemyNumber (evalSchemyDouble (eval map a) + evalSchemyDouble (eval map b))
+eval map (SchemyMult a b) = SchemyNumber (evalSchemyDouble (eval map a) * evalSchemyDouble (eval map b))
 eval map (SchemySymbol s) = map ! s
+
+evalSchemyDouble :: SchemyExp -> Double
+evalSchemyDouble (SchemyNumber a) = a
+
+--let env = fromList[("pi",pi)]
